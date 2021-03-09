9424d3e0ab6e6672dbe30ea8e6b3b80c061f8122
K7 <chenglang@k7cl.com>
2021-03-09 09:24:35 +0800

    add users list

--- src/ityxbAutoSign/Main.java
+++ src/ityxbAutoSign/Main.java
@@ -1,16 +1,28 @@
 package ityxbAutoSign;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+	public static List<AccountUtil> users = new ArrayList<>();
 
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
+		
 		AccountUtil a1 = new AccountUtil();
-		a1.login("your username here", "your password here");
-		TaskUtil t1 = new TaskUtil();
+		a1.login("your username1 here", "password for username1");
+		users.add(a1);
+		
+		AccountUtil a2 = new AccountUtil();
+		a2.login("your username2 here", "password for username2");
+		users.add(a2);
+		
 		while (true){
-			System.out.println("=============================================================================================");
-			t1.process(a1);
+			for (int i=0;i<users.size();i++) {
+				TaskUtil t = new TaskUtil();
+				System.out.println("=============================================================================================");
+				t.process(users.get(i));
+			}
 			try {
 				Thread.sleep(5000);
 			} catch (InterruptedException e) {
